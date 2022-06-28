5925c6a2ffeb8cdb5ece1286ab0c134588bd1acf
Kunyuan Liu <kunyuan.liu@ibm.com>
2022-06-28 15:41:09 -0700

    random z_init, fix jac

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ import scipy.optimize as so
 
 
 class RateConstantModel():
-    def __init__(self, num_species= 2, num_reactions=3, rates = [0.1, 0.05, 0.05], alpha=0.0, lamb=1.0, method='SLSQP', tol=1e-16, approx_jac = False):
+    def __init__(self, num_species=2, num_reactions=3, rates=[0.1, 0.05, 0.05], alpha=0.0, lamb=1.0, method='SLSQP',
+                 tol=1e-16, approx_jac=False):
         self.alpha = alpha
         self.lamb = lamb
         self.N = num_species
@@ -14,49 +15,46 @@ class RateConstantModel():
         self.tol = tol
         self.approx_jac = approx_jac
 
-
-        #self.rates
+        # self.rates
 
     def compute_theta(self, Z):
-
+        '''
+        ##Brusselator
         ##just hard coding A and B for now
         A = 1
-        B = 1.7 ### remove hard code later
+        B = 1.7  ### remove hard code later
         y1 = []
         for i in range(len(Z[:, 0])):
             y1.append(np.transpose([A, 0]))
         y1 = np.array(y1)
 
-        y2 = np.array(np.transpose([Z[:, 0]**2 * Z[:, 1], -Z[:, 0]**2 * Z[:, 1]]))
-        y3 = np.array(np.transpose([- B*Z[:, 0], B*Z[:, 0]]))
+        y2 = np.array(np.transpose([Z[:, 0] ** 2 * Z[:, 1], -Z[:, 0] ** 2 * Z[:, 1]]))
+        y3 = np.array(np.transpose([- B * Z[:, 0], B * Z[:, 0]]))
 
         y4 = []
         for X in Z[:, 0]:
             y4.append(np.transpose([-X, 0]))
         y4 = np.array(y4)
         theta = np.transpose([y1, y2, y3, y4], (1, 0, 2))
-        #print("y4",y4)
-        #print("theta",theta.shape)
+        # print("y4",y4)
+        # print("theta",theta.shape)
         return theta
 
         '''
         y1 = [] #Lotka Volterra
         for z in Z[:, 0]:
             y1.append(np.transpose([z, 0])) 
-
         y1 = np.array(y1)
-
         y2 = []
         for z in Z[:, 1]:
             y2.append(np.transpose([0, -z])) 
         y2 = np.array(y2)
-
         y3 = np.array(np.transpose([- Z[:, 0] * Z[:, 1], Z[:, 0] * Z[:, 1]]))
         theta = np.transpose([y1, y2, y3], (1, 0, 2))
         return theta
-        '''
 
-    def elastic_net_func(self, propensities,  Z_arr, theta_arr, dt, alpha, lamb):
+
+    def elastic_net_func(self, propensities, Z_arr, theta_arr, dt, alpha, lamb):
 
         num_species = self.N
         num_reactions = self.R
@@ -67,14 +65,15 @@ class RateConstantModel():
         for i in range(len(theta_arr)):
             theta = theta_arr[i]
             time_steps = len(theta)
-            Z = Z_arr[i] * int(1/dt)
-            dZ = np.gradient(Z)  # Look into this later, returns an array of size 2x100x2, for now I just choose dZ[0] ###dt = 0.01 hard code 100
+            Z = Z_arr[i] * int(1 / dt)
+            dZ = np.gradient(
+                Z)  # Look into this later, returns an array of size 2x100x2, for now I just choose dZ[0] ###dt = 0.01 hard code 100
             for t in range(time_steps):
                 for s in range(num_species):
-                    x = dZ[0][t][s] #dZ
+                    x = dZ[0][t][s]  # dZ
                     for r in range(num_reactions):
                         x -= propensities[r] * theta[t][r][s]
-                    result += x**2
+                    result += x ** 2
 
             total_time_steps += time_steps
 
@@ -91,45 +90,46 @@ class RateConstantModel():
         if alpha != 0 and lamb < 1.0:
             l2_regulator = 0
             for r in range(num_reactions):
-                l2_regulator += propensities[r]**2
-            l2_regulator *= alpha * (1-lamb)
+                l2_regulator += propensities[r] ** 2
+            l2_regulator *= alpha * (1 - lamb)
             regulator += l2_regulator
 
         return result + regulator
 
-    def elastic_net_jac(self, propensities,  Z_arr, theta_arr, alpha, lamb):
+    def elastic_net_jac(self, propensities, Z_arr, theta_arr, dt, alpha, lamb):
 
         num_species = self.N
         num_reactions = self.R
 
-        result = []
+        result = np.zeros(num_reactions)
         total_time_steps = 0
 
         for i in range(len(theta_arr)):
             theta = theta_arr[i]
             time_steps = len(theta)
-            Z = Z_arr[i]
+            Z = Z_arr[i] * int(1 / dt)
             dZ = np.gradient(Z)  # Look into this later, returns an array of size 2x100x2, for now I just choose dZ[0]
             total_time_steps += time_steps
             for j in range(num_reactions):
+
                 for t in range(time_steps):
                     for s in range(num_species):
-                        x = dZ[0][t][s] #dZ
+                        x = dZ[0][t][s]  # dZ
                         theta_t_j_s = theta[t][j][s]
                         for r in range(num_reactions):
                             x -= propensities[r] * theta[t][r][s]
+
                         result[j] += theta_t_j_s * x
-            
+
         result /= -total_time_steps
 
-
         for j in range(num_reactions):
-            #l1 regulator
-            result[i] += alpha * lamb
+            # l1 regulator
+            result[j] += alpha * lamb
             # l2 regulator
-            result[i] += 2 * alpha * (1-lamb) * propensities[i]
+            result[j] += 2 * alpha * (1 - lamb) * propensities[j]
 
-
+        return result
 
     def solve_minimize(self, Z_arr, theta_arr, dt):
         def objective(x):
@@ -137,7 +137,7 @@ class RateConstantModel():
             return obj
 
         jac = False if self.approx_jac else \
-            lambda x: self.elastic_net_jac(x, Z_arr, theta_arr, self.alpha, self.lamb)
+        lambda x: self.elastic_net_jac(x, Z_arr, theta_arr, dt,self.alpha, self.lamb)
 
         result = so.minimize(
             objective,
@@ -145,7 +145,6 @@ class RateConstantModel():
             bounds=None,
             tol=self.tol,
             method=self.method,
-            jac=None,
+            jac=jac,
             options=None)
         return result
-
